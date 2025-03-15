da71b1e9b285a218e7b91269942d04402179bdf0
Lian-040400 <xacatryanlianna630@gmail.com>
2025-03-15 09:03:22 +0400

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-﻿const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config({path: './config.env'});
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+﻿const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: true,
-    })
-    .then(() => console.log("✅ DB connected successfully."))
-    .catch((err) => console.error("❌ DB connection failed:", err.message));
-const app = require('./app')
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(() => console.log('✅ DB connected successfully.'))
+  .catch((err) => console.error('❌ DB connection failed:', err.message));
+const app = require('./app');
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: 'Number',
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'To Duba 1',
+  rating: 4.5,
+  price: 497
+});
+testTour.save().then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log('Error: ', err);
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
